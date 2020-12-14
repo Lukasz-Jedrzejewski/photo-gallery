3c93f346fca19ab2f4b2d929edc42698a87a96c2
Lukasz-Jedrzejewski <generaljedrzejewski@gmail.com>
2020-12-14 15:00:34 +0100

    add security configuration

---
+++ src/main/java/com/jedrzejewski/photogallery/configuration/SecurityConfig.java
@@ -0,0 +1,51 @@
+package com.jedrzejewski.photogallery.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/", "/register").permitAll()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/user/**").hasRole("USER")
+                .and().formLogin()
+                .loginPage("/login")
+                .successHandler(authenticationSuccessHandler())
+                .and().logout().logoutSuccessUrl("/")
+                .permitAll()
+                .and().exceptionHandling().accessDeniedPage("/403");
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler authenticationSuccessHandler () {
+        return (request, response, authentication) -> {
+            if (authentication.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_ADMIN"))) {
+                response.sendRedirect("/admin/panel");
+            } else {
+                response.sendRedirect("/user/panel");
+            }
+        };
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SpringDataUserDetailsService customUserDetailsService() {
+        return new SpringDataUserDetailsService();
+    }
+}
